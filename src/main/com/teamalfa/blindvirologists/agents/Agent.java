1af2f6cfbb9da3f8e98fe9067acc0724d9d0e3a3
krisztianszenasi <szenasi999@gmail.com>
2022-03-11 00:37:29 +0100

    ✨feat(Agent): Added target and genetic code attributes to Agent.

--- src/main/com/teamalfa/blindvirologists/agents/Agent.java
+++ src/main/com/teamalfa/blindvirologists/agents/Agent.java
@@ -1,5 +1,14 @@
 package main.com.teamalfa.blindvirologists.agents;
 
-abstract public class Agent {
+import main.com.teamalfa.blindvirologists.Virologist;
+import main.com.teamalfa.blindvirologists.agents.geneticCodes.GeneticCode;
 
+abstract public class Agent {
+    protected Virologist target = null;
+    protected GeneticCode code = null;
+
+    abstract public void applyTo(Virologist virologist);
+    public GeneticCode getCode() {
+        return code;
+    }
 }
